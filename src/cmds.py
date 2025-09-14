fbb61ddba3eba4e2efab55e2debb363757e61508
ARagdollCat <115324965+ACaracalCat@users.noreply.github.com>
2025-09-13 20:24:27 -0500

    VERSION / ver

--- src/cmds.py
+++ src/cmds.py
@@ -1,11 +1,13 @@
 import os
 
 from . import vfs
+from .version import VERSION
 
 cmds = {
     "exit": lambda args: exit(),
     "clear": lambda args: os.system("cls" if os.name == "nt" else "clear"),
     "echo": lambda args: print(" ".join(args)),
+    "ver": lambda args: print(f"{VERSION}"),
     "tree": lambda args: vfs.tree(),
     "ls": lambda args: vfs.ls(),
     "pwd": lambda args: print(vfs.pwd()),
